ad7184da0e768090360bc34b0e84a05c905b20cd
hamade-kj <hamadekj15@gmail.com>
2023-01-22 01:48:24 +0100

    get update

--- src/main/java/chat/Room.java
+++ src/main/java/chat/Room.java
@@ -2,20 +2,35 @@ package chat;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Room extends Application {
     
+    public static void main(String[] args) {
+        launch(args);
+  }
+
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Room Chat");
         BorderPane borderPane = new BorderPane();
+        
+        //creating checkboxes for rentre dans une chat room available
+
+        
+
+        //creation de notre button qui login a l'un de room available
+        Button logininRoom = new Button("");
+        logininRoom.setFont(Font.font("Comic Sans MS"));
 
 
         Scene sceneRoom = new Scene(borderPane,900,600);
         stage.setScene(sceneRoom);
+        stage.setResizable(false);
         stage.show();
     }
 }
