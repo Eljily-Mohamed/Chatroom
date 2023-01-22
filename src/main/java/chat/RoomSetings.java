271386bb0fb2d19653338f5a37690b6c809f8aa4
hamade-kj <hamadekj15@gmail.com>
2023-01-22 12:17:13 +0100

    get update

--- src/main/java/chat/RoomSetings.java
+++ src/main/java/chat/RoomSetings.java
@@ -18,6 +18,7 @@ import javafx.scene.control.Separator;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -35,6 +36,7 @@ public class RoomSetings extends Application {
         stage.setTitle("Room Chat");
          Text textRoom = new Text("Room Available: ");
 		 textRoom.setFont(Font.font("Comic Sans MS"));
+       
 
         //cree text for display for identifie le room available 
 
@@ -72,42 +74,67 @@ public class RoomSetings extends Application {
        
         cb.setOnAction(event);
 
-        System.out.println("item selectione : "+item);
+      
         //creation de notre button qui login a l'un de room available
         Button logininRoom = new Button("Login");
         logininRoom.setFont(Font.font("Comic Sans MS"));
          
         //ajout logique pour la recuperation de la valeur selectione par user
         
-        if(!selected.getText().isEmpty()){
+       
             logininRoom.setOnAction((Event) -> {
+                if(!selected.getText().isEmpty()){
                 String[] words = selected.getText().split("\\W");
                     System.out.println(words[0]);
+                }
+                else{
+                    System.out.println("not Room selectione ");
+                }
             });     
-        }  
+       
+        HBox hboxRoom = new HBox();
+        hboxRoom.setSpacing(10);
+        hboxRoom.setStyle("-fx-background-color: #DAF7A6 ;"); 
+        hboxRoom.setPadding(new Insets(10,20,20,10));
+        hboxRoom.getChildren().addAll(selected,textRoom,cb,logininRoom);
+        hboxRoom.setAlignment(Pos.CENTER);
+        
+    
+        //creation for buttons chat Private  
+
+              //create ComboBox 
+              String [] privateChoix = {"PV -> Numero","PV -> Name"};
+              ComboBox cbPrivate =
+              new ComboBox(FXCollections
+                        .observableArrayList(privateChoix));
+            
+                        Label selectedPrivate = new Label();
+        
+                        //create handler for this comboBox 
+                        EventHandler<ActionEvent> eventPrivate =
+                        new EventHandler<ActionEvent>() {
+                             public void handle(ActionEvent e){
+                                selected.setText(cb.getValue() + " selected");
+                            }
+                        };
+                       
+                        cb.setOnAction(eventPrivate);
+    
+        Button logininPrivate = new Button("Login");
+        logininPrivate.setFont(Font.font("Comic Sans MS"));
         
         HBox hboxPrivate = new HBox();
         hboxPrivate.setSpacing(10);
         hboxPrivate.setStyle("-fx-background-color: #DAF7A6 ;"); 
         hboxPrivate.setPadding(new Insets(10,20,20,10));
-        hboxPrivate.getChildren().addAll(selected,textRoom,cb,logininRoom);
+        hboxPrivate.getChildren().addAll(cbPrivate,logininPrivate);
         hboxPrivate.setAlignment(Pos.CENTER);
         
-         
-        //creation for buttons chat Private  
-    
-        Button logininPrivate = new Button("Login");
-        logininPrivate.setFont(Font.font("Comic Sans MS"));
+        VBox vboxroot = new VBox(hboxRoom,hboxPrivate);
+        vboxroot.setAlignment(Pos.CENTER);
 
-        HBox hboxRoom = new HBox();
-        hboxRoom.setSpacing(10);
-        hboxRoom.setStyle("-fx-background-color: #DAF7A6 ;"); 
-        hboxRoom.setPadding(new Insets(10,20,20,10));
-        hboxRoom.getChildren().addAll(,cb,logininPrivate);
-        hboxRoom.setAlignment(Pos.CENTER);
-
-
-        Scene sceneRoom = new Scene(hboxRoom,900,600);
+        
+        Scene sceneRoom = new Scene(vboxroot,900,600);
         stage.setScene(sceneRoom);
         stage.setResizable(false);
         stage.show();
