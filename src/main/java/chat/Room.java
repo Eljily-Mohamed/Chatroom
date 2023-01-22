bfc28808e9692dfde1912d443943eb1f57bddf98
hamade-kj <hamadekj15@gmail.com>
2023-01-22 01:28:31 +0100

    get update

--- src/main/java/chat/Room.java
+++ src/main/java/chat/Room.java
@@ -1,12 +1,21 @@
 package chat;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Room extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        
+        stage.setTitle("Room Chat");
+        BorderPane borderPane = new BorderPane();
+
+
+        Scene sceneRoom = new Scene(borderPane,900,600);
+        stage.setScene(sceneRoom);
+        stage.show();
     }
 }
