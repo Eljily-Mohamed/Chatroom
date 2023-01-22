be8424f678f9b0f17254698e5702d8ca2076d60b
hamade-kj <hamadekj15@gmail.com>
2023-01-22 01:53:58 +0100

    get update

--- src/main/java/chat/Room.java
+++ src/main/java/chat/Room.java
@@ -1,10 +1,15 @@
 package chat;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -21,12 +26,22 @@ public class Room extends Application {
         
         //creating checkboxes for rentre dans une chat room available
 
-        
+        ChoiceBox cb = new ChoiceBox();
+        cb.setItems(FXCollections.observableArrayList(
+            "New Document", "Open ", 
+            new Separator(), "Save", "Save as")
+        );
 
         //creation de notre button qui login a l'un de room available
         Button logininRoom = new Button("");
         logininRoom.setFont(Font.font("Comic Sans MS"));
-
+         
+        HBox hboxRoom = new HBox();
+        hboxRoom.setSpacing(10);
+        hboxRoom.setStyle("-fx-background-color: #DAF7A6 ;"); 
+        hboxRoom.setPadding(new Insets(10,20,20,10));
+        hboxRoom.getChildren().addAll(cb,logininRoom);
+      
 
         Scene sceneRoom = new Scene(borderPane,900,600);
         stage.setScene(sceneRoom);
