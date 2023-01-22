73ec4ae05a39a6a66c753262d3568fb502078d0f
hamade-kj <hamadekj15@gmail.com>
2023-01-22 02:03:24 +0100

    get Update

--- src/main/java/chat/Room.java
+++ src/main/java/chat/Room.java
@@ -22,8 +22,8 @@ public class Room extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Room Chat");
-        BorderPane borderPane = new BorderPane();
         
+        //
         //creating checkboxes for rentre dans une chat room available
 
         ChoiceBox cb = new ChoiceBox();
@@ -33,7 +33,7 @@ public class Room extends Application {
         );
 
         //creation de notre button qui login a l'un de room available
-        Button logininRoom = new Button("");
+        Button logininRoom = new Button("Login");
         logininRoom.setFont(Font.font("Comic Sans MS"));
          
         HBox hboxRoom = new HBox();
@@ -41,11 +41,9 @@ public class Room extends Application {
         hboxRoom.setStyle("-fx-background-color: #DAF7A6 ;"); 
         hboxRoom.setPadding(new Insets(10,20,20,10));
         hboxRoom.getChildren().addAll(cb,logininRoom);
-          
-        borderPane.setCenter(hboxRoom);
-        borderPane.setScaleX(200);
-        
-        Scene sceneRoom = new Scene(borderPane,900,600);
+
+      
+        Scene sceneRoom = new Scene(hboxRoom,900,600);
         stage.setScene(sceneRoom);
         stage.setResizable(false);
         stage.show();
