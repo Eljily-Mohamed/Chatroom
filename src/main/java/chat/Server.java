128959f50eb7966e7ec959948affcd336fca2e74
hamade-kj <hamadekj15@gmail.com>
2023-01-25 12:44:53 +0100

    get update

--- src/main/java/chat/Server.java
+++ src/main/java/chat/Server.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -31,14 +33,26 @@ public class Server extends Thread {
 	  @Override
 	public void run() {
           try {
-			ServerSocket serveurSocket = new ServerSocket(1234);
-			Socket socket = serveurSocket.accept();
-				    //on doit passe room in the conversation 
+            ServerSocket ss = new ServerSocket(1234);
+            System.out.println("le systeme est pret pour accepter les connexions");
+            Socket socket = ss.accept();
+            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+    
+            Room m = (Room) is.readObject();
+           
+            System.out.println(m);
+    
+            os.writeObject(m);
+            socket.close(); 
                
 			} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 	 }
   }
 	  
