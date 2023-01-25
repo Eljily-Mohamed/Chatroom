b55c748aa0bd9d02a137cbc5a33a6481706c6292
hamade-kj <hamadekj15@gmail.com>
2023-01-25 12:22:14 +0100

    get update

--- src/main/java/chat/Server.java
+++ src/main/java/chat/Server.java
@@ -10,7 +10,11 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-//on doit cree notre server multithread 
+//on doit cree notre server multithread
+
+
+//class server for 
+
 public class Server extends Thread {
 	
 	  PrintWriter pw;
@@ -18,12 +22,9 @@ public class Server extends Thread {
 	  InputStreamReader isr ;
 	  BufferedReader br ;
 	  
-	  private boolean isActive = true;
-	  private int nmbr_Clients= 0 ;
-	  private List<String> nomUsres = new ArrayList<String>();
-	  private List<Conversation> clients = new ArrayList<Conversation>();
-	  
-	  
+
+	  private List<Room> rooms = new ArrayList<Room>();
+
 	  public static void main(String[] args) {
 		new Serveur().start();
 	}
@@ -31,98 +32,14 @@ public class Server extends Thread {
 	public void run() {
           try {
 			ServerSocket serveurSocket = new ServerSocket(1234);
-			while(isActive) {
-				Socket socket = serveurSocket.accept();
-				++nmbr_Clients;	
-				Conversation conversation = new Conversation(socket,nmbr_Clients);
-				clients.add(conversation);
-				conversation.start();
-			}
-		} catch (IOException e) {
+			Socket socket = serveurSocket.accept();
+				    //on doit passe room in the conversation 
+               
+			} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	  
-//////////////////class conversation 
-	  
-	 class Conversation extends Thread{
-		 protected Socket socket_client ;
-		 protected int numero_client;
-		 
-		 //on doit ajoute tab qui contient le noms de users
-		 public Conversation(Socket socket_client , int numero_client) {
-			this.socket_client = socket_client;
-			this.numero_client = numero_client;
-		}
-		 
-		//public void brodcast_message(String message,Socket socket, String nomUser)
-		 public void brodcast_message(String message,Socket socket){
-			
-		//for(String nomClient:nomUsres) {
-			 for (Conversation client:clients) {
-				 try {
-					  //if (nomClient.equals(nomUser)) {
-					    if(client.socket_client != socket) {
-					    	 pw = new PrintWriter(client.socket_client.getOutputStream(),true);
-							 pw.println(message); 
-							   break;
-					    } 
-//					    else {
-//					    	 pw = new PrintWriter(client.socket_client.getOutputStream(),true);
-//							 pw.println(message); 
-//					    }
-					 
-					  //}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				 
-			  //}
-			} 
-		
-		//messgae vers tous monde 
-		
-		}
-		  @Override
-			public void run() {
-				try {
-					is = socket_client.getInputStream();
-			        isr = new InputStreamReader(is);
-				    br = new BufferedReader(isr);
-					
-					pw = new PrintWriter(socket_client.getOutputStream(),true);
-					String ipClinet = socket_client.getRemoteSocketAddress().toString();
-					pw.println("Bien venue , vous etes le client"+ipClinet);
-					System.out.println("le connexion bien etablie a client :"+ipClinet);
-					//pw.println("Ajout nom ici : ");
-					//nomUsres.add(br.readLine());
-					
-					while(true) {
-						String req=br.readLine();
-						System.out.println(req);
-						String req_Reponse[] = req.split(":");
-						System.out.println(req);
-						if(req_Reponse.length == 2) {
-							String nomClient = req_Reponse[0];
-							String message = req_Reponse[1];
-							 //brodcast_message(message,socket_client,nomClient);	
-							   brodcast_message(message,socket_client);
-						}
-						else {
-							 //brodcast_message(req,socket_client,"unll");
-							   brodcast_message(req,socket_client);
-						}
-					}
-					
-					
-					
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
 	 }
+  }
 	  
-}
+//end class etablier connexion 
